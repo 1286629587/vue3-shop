bb10b1c4ca8e374211d72e2f8b650cace2aea845
hongzida <1286629587@qq.com>
2022-05-15 22:00:38 +0800

    使用静态菜单数据

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -25,7 +25,7 @@
           :default-active="activePath"
         >
           <!-- 一级菜单 -->
-          <el-submenu :index="item.id+''" :key="item.id" v-for="item in menuList">
+          <el-submenu :index="item.id+''" :key="item.id" v-for="item in sData">
             <!-- 一级菜单模板区域 -->
             <template #title>
               <!-- 图标 -->
@@ -67,7 +67,7 @@ import { getCurrentInstance, reactive, ref } from 'vue'
 import { Menu, Avatar, Grid, Shop, List, TrendCharts } from '@element-plus/icons-vue'
 
 const { proxy } = getCurrentInstance()
-const { menuList } = reactive({ menuList: [] })
+// const { menuList } = reactive({ menuList: [] })
 // 是否折叠
 const isCollapse = ref(false)
 // 用于保存激活状态的菜单地址
@@ -80,16 +80,94 @@ const logout = () => {
   // 跳转到登陆页
   proxy.$router.push('/login')
 }
-
-// 获取菜单数据，保存至menuList
-const getMemuList = async () => {
-  const { data } = await proxy.$http.get('menus')
-  if (data.meta.status !== 200) return proxy.$message.error(data.meta.msg)
-  for (const key in data.data) {
-    menuList.push(data.data[key])
+// 静态表单数据
+const sData = reactive([
+  {
+    id: 125,
+    authName: '用户管理',
+    path: 'users',
+    children: [
+      {
+        id: 110,
+        authName: '用户列表',
+        path: 'users'
+      }
+    ]
+  },
+  {
+    id: 103,
+    authName: '权限管理',
+    path: 'rights',
+    children: [
+      {
+        id: 111,
+        authName: '角色列表',
+        path: 'roles'
+      },
+      {
+        id: 112,
+        authName: '权限列表',
+        path: 'rights'
+      }
+    ]
+  },
+  {
+    id: 101,
+    authName: '商品管理',
+    path: 'goods',
+    children: [
+      {
+        id: 104,
+        authName: '商品列表',
+        path: 'goods'
+      },
+      {
+        id: 115,
+        authName: '分类参数',
+        path: 'params'
+      },
+      {
+        id: 121,
+        authName: '商品分类',
+        path: 'categories'
+      }
+    ]
+  },
+  {
+    id: 102,
+    authName: '订单管理',
+    path: 'orders',
+    children: [
+      {
+        id: 107,
+        authName: '订单列表',
+        path: 'orders'
+      }
+    ]
+  },
+  {
+    id: 145,
+    authName: '数据统计',
+    path: 'reports',
+    children: [
+      {
+        id: 146,
+        authName: '数据报表',
+        path: 'reports'
+      }
+    ]
   }
-}
-getMemuList()
+])
+console.log(sData)
+// 获取菜单数据，保存至menuList
+// const getMemuList = async () => {
+//   const { data } = await proxy.$http.get('menus')
+//   if (data.meta.status !== 200) return proxy.$message.error(data.meta.msg)
+//   for (const key in data.data) {
+//     menuList.push(data.data[key])
+//   }
+// }
+// getMemuList()
 
 // 点击按钮，切换菜单的折叠与展开
 const toggleCollapse = () => {
