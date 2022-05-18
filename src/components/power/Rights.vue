d000e999a4793dd3a818b7492c44256c5540a478
hongzida <1286629587@qq.com>
2022-05-18 13:22:10 +0800

    数据统计，权限列表静态数据

--- src/components/power/Rights.vue
+++ src/components/power/Rights.vue
@@ -7,15 +7,20 @@
   </el-breadcrumb>
   <!-- 卡片视图区 -->
   <el-card>
-    <el-table
+    <!-- <el-table
      :border="true"
      :data="list.rightsList"
      :stripe="true"
+    > -->
+    <el-table
+     :border="true"
+     :data="slist"
+     :stripe="true"
     >
       <el-table-column type="index" label="#"></el-table-column>
-      <el-table-column label="权限名称" prop="authName"></el-table-column>
-      <el-table-column label="路径" prop="path"></el-table-column>
-      <el-table-column label="权限等级" prop="level">
+      <el-table-column label="权限名称" prop=authName></el-table-column>
+      <el-table-column label="路径" prop=path></el-table-column>
+      <el-table-column label="权限等级" prop=level>
         <template v-slot="scope">
           <el-tag v-if="scope.row.level === '0'">一级</el-tag>
           <el-tag type="success" v-if="scope.row.level === '1'">二级</el-tag>
@@ -32,7 +37,345 @@ import { getCurrentInstance, reactive } from 'vue'
 const { proxy } = getCurrentInstance()
 // 权限列表
 const list = reactive({ rightsList: [] })
-// 添加角色
+// 静态权限列表数据
+const slist = reactive([
+  {
+    id: 101,
+    authName: '商品管理',
+    level: '0',
+    pid: 0,
+    path: 'goods'
+  },
+  {
+    id: 102,
+    authName: '订单管理',
+    level: '0',
+    pid: 0,
+    path: 'orders'
+  },
+  {
+    id: 103,
+    authName: '权限管理',
+    level: '0',
+    pid: 0,
+    path: 'rights'
+  },
+  {
+    id: 104,
+    authName: '商品列表',
+    level: '1',
+    pid: 101,
+    path: 'goods'
+  },
+  {
+    id: 105,
+    authName: '添加商品',
+    level: '2',
+    pid: 104,
+    path: 'goods'
+  },
+  {
+    id: 107,
+    authName: '订单列表',
+    level: '1',
+    pid: 102,
+    path: 'orders'
+  },
+  {
+    id: 109,
+    authName: '添加订单',
+    level: '2',
+    pid: 107,
+    path: 'orders'
+  },
+  {
+    id: 110,
+    authName: '用户列表',
+    level: '1',
+    pid: 125,
+    path: 'users'
+  },
+  {
+    id: 111,
+    authName: '角色列表',
+    level: '1',
+    pid: 103,
+    path: 'roles'
+  },
+  {
+    id: 112,
+    authName: '权限列表',
+    level: '1',
+    pid: 103,
+    path: 'rights'
+  },
+  {
+    id: 115,
+    authName: '分类参数',
+    level: '1',
+    pid: 101,
+    path: 'params'
+  },
+  {
+    id: 116,
+    authName: '商品修改',
+    level: '2',
+    pid: 104,
+    path: 'goods'
+  },
+  {
+    id: 117,
+    authName: '商品删除',
+    level: '2',
+    pid: 104,
+    path: 'goods'
+  },
+  {
+    id: 121,
+    authName: '商品分类',
+    level: '1',
+    pid: 101,
+    path: 'categories'
+  },
+  {
+    id: 122,
+    authName: '添加分类',
+    level: '2',
+    pid: 121,
+    path: 'categories'
+  },
+  {
+    id: 123,
+    authName: '删除分类',
+    level: '2',
+    pid: 121,
+    path: 'categories'
+  },
+  {
+    id: 125,
+    authName: '用户管理',
+    level: '0',
+    pid: 0,
+    path: 'users'
+  },
+  {
+    id: 129,
+    authName: '添加角色',
+    level: '2',
+    pid: 111,
+    path: 'roles'
+  },
+  {
+    id: 130,
+    authName: '删除角色',
+    level: '2',
+    pid: 111,
+    path: 'roles'
+  },
+  {
+    id: 131,
+    authName: '添加用户',
+    level: '2',
+    pid: 110,
+    path: 'users'
+  },
+  {
+    id: 132,
+    authName: '删除用户',
+    level: '2',
+    pid: 110,
+    path: 'users'
+  },
+  {
+    id: 133,
+    authName: '更新用户',
+    level: '2',
+    pid: 110,
+    path: 'users'
+  },
+  {
+    id: 134,
+    authName: '角色授权',
+    level: '2',
+    pid: 111,
+    path: 'roles'
+  },
+  {
+    id: 135,
+    authName: '取消角色授权',
+    level: '2',
+    pid: 111,
+    path: 'roles'
+  },
+  {
+    id: 136,
+    authName: '获取用户详情',
+    level: '2',
+    pid: 110,
+    path: 'users'
+  },
+  {
+    id: 137,
+    authName: '分配用户角色',
+    level: '2',
+    pid: 110,
+    path: 'users'
+  },
+  {
+    id: 138,
+    authName: '获取角色列表',
+    level: '2',
+    pid: 111,
+    path: 'roles'
+  },
+  {
+    id: 139,
+    authName: '获取角色详情',
+    level: '2',
+    pid: 111,
+    path: 'roles'
+  },
+  {
+    id: 140,
+    authName: '更新角色信息',
+    level: '2',
+    pid: 111,
+    path: 'roles'
+  },
+  {
+    id: 141,
+    authName: '更新角色权限',
+    level: '2',
+    pid: 111,
+    path: 'roles'
+  },
+  {
+    id: 142,
+    authName: '获取参数列表',
+    level: '2',
+    pid: 115,
+    path: 'categories'
+  },
+  {
+    id: 143,
+    authName: '创建商品参数',
+    level: '2',
+    pid: 115,
+    path: 'categories'
+  },
+  {
+    id: 144,
+    authName: '删除商品参数',
+    level: '2',
+    pid: 115,
+    path: 'categories'
+  },
+  {
+    id: 145,
+    authName: '数据统计',
+    level: '0',
+    pid: 0,
+    path: 'reports'
+  },
+  {
+    id: 146,
+    authName: '数据报表',
+    level: '1',
+    pid: 145,
+    path: 'reports'
+  },
+  {
+    id: 147,
+    authName: '查看权限',
+    level: '2',
+    pid: 112,
+    path: 'rights'
+  },
+  {
+    id: 148,
+    authName: '查看数据',
+    level: '2',
+    pid: 146,
+    path: 'reports'
+  },
+  {
+    id: 149,
+    authName: '获取分类详情',
+    level: '2',
+    pid: 121,
+    path: 'categories'
+  },
+  {
+    id: 150,
+    authName: '更新商品图片',
+    level: '2',
+    pid: 104,
+    path: 'goods'
+  },
+  {
+    id: 151,
+    authName: '更新商品属性',
+    level: '2',
+    pid: 104,
+    path: 'goods'
+  },
+  {
+    id: 152,
+    authName: '更新商品状态',
+    level: '2',
+    pid: 104,
+    path: 'goods'
+  },
+  {
+    id: 153,
+    authName: '获取商品详情',
+    level: '2',
+    pid: 104,
+    path: 'goods'
+  },
+  {
+    id: 154,
+    authName: '订单更新',
+    level: '2',
+    pid: 107,
+    path: 'orders'
+  },
+  {
+    id: 155,
+    authName: '获取订单详情',
+    level: '2',
+    pid: 107,
+    path: 'orders'
+  },
+  {
+    id: 156,
+    authName: '分类参数添加',
+    level: '2',
+    pid: 101,
+    path: 'categories'
+  },
+  {
+    id: 157,
+    authName: '分类参数删除',
+    level: '2',
+    pid: 101,
+    path: 'categories'
+  },
+  {
+    id: 158,
+    authName: '分类参数详情',
+    level: '2',
+    pid: 101,
+    path: 'categories'
+  },
+  {
+    id: 159,
+    authName: '设置管理状态',
+    level: '2',
+    pid: 110,
+    path: 'users'
+  }
+])
 
 // 发起请求，获取权限列表数据
 const getRightsList = async () => {
--- src/components/report/Report.vue
+++ src/components/report/Report.vue
@@ -12,53 +12,190 @@
 </template>
 
 <script setup>
-import { getCurrentInstance, onMounted, reactive } from 'vue'
+// import { getCurrentInstance, onMounted, reactive } from 'vue'
+import { onMounted, reactive } from 'vue'
 import * as echarts from 'echarts'
-import merge from 'deepmerge'
-const { proxy } = getCurrentInstance()
+// import merge from 'deepmerge'
+// const { proxy } = getCurrentInstance()
 // 需要合并的数据
-const { options } = reactive({
-  options: {
-    title: {
-      text: '用户来源'
-    },
-    tooltip: {
-      trigger: 'axis',
-      axisPointer: {
-        type: 'cross',
-        label: {
-          backgroundColor: '#E9EEF3'
-        }
-      }
-    },
-    grid: {
-      left: '3%',
-      right: '4%',
-      bottom: '3%',
-      containLabel: true
-    },
-    xAxis: [
-      {
-        boundaryGap: false
-      }
-    ],
-    yAxis: [
-      {
-        type: 'value'
-      }
+// const { options } = reactive({
+//   options: {
+//     title: {
+//       text: '用户来源'
+//     },
+//     tooltip: {
+//       trigger: 'axis',
+//       axisPointer: {
+//         type: 'cross',
+//         label: {
+//           backgroundColor: '#E9EEF3'
+//         }
+//       }
+//     },
+//     grid: {
+//       left: '3%',
+//       right: '4%',
+//       bottom: '3%',
+//       containLabel: true
+//     },
+//     xAxis: [
+//       {
+//         boundaryGap: false
+//       }
+//     ],
+//     yAxis: [
+//       {
+//         type: 'value'
+//       }
+//     ]
+//   }
+// })
+// 静态合并后的数据
+const sNewObj = reactive({
+  legend: {
+    data: [
+      '华东',
+      '华南',
+      '华北',
+      '西部',
+      '其他'
     ]
+  },
+  yAxis: [
+    {
+      type: 'value'
+    },
+    {
+      type: 'value'
+    }
+  ],
+  xAxis: [
+    {
+      data: [
+        '2017-12-27',
+        '2017-12-28',
+        '2017-12-29',
+        '2017-12-30',
+        '2017-12-31',
+        '2018-1-1'
+      ]
+    },
+    {
+      boundaryGap: false
+    }
+  ],
+  series: [
+    {
+      name: '华东',
+      type: 'line',
+      stack: '总量',
+      areaStyle: {
+        normal: {}
+      },
+      data: [
+        2999,
+        3111,
+        4100,
+        3565,
+        3528,
+        6000
+      ]
+    },
+    {
+      name: '华南',
+      type: 'line',
+      stack: '总量',
+      areaStyle: {
+        normal: {}
+      },
+      data: [
+        5090,
+        2500,
+        3400,
+        6000,
+        6400,
+        7800
+      ]
+    },
+    {
+      name: '华北',
+      type: 'line',
+      stack: '总量',
+      areaStyle: {
+        normal: {}
+      },
+      data: [
+        6888,
+        4000,
+        8010,
+        12321,
+        13928,
+        12984
+      ]
+    },
+    {
+      name: '西部',
+      type: 'line',
+      stack: '总量',
+      areaStyle: {
+        normal: {}
+      },
+      data: [
+        9991,
+        4130,
+        7777,
+        12903,
+        13098,
+        14028
+      ]
+    },
+    {
+      name: '其他',
+      type: 'line',
+      stack: '总量',
+      areaStyle: {
+        normal: {}
+      },
+      data: [
+        15212,
+        5800,
+        10241,
+        14821,
+        15982,
+        14091
+      ]
+    }
+  ],
+  title: {
+    text: '用户来源'
+  },
+  tooltip: {
+    trigger: 'axis',
+    axisPointer: {
+      type: 'cross',
+      label: {
+        backgroundColor: '#E9EEF3'
+      }
+    }
+  },
+  grid: {
+    left: '3%',
+    right: '4%',
+    bottom: '3%',
+    containLabel: true
   }
 })
-
-onMounted(async () => {
+// onMounted(async () => {
+onMounted(() => {
   // 基于准备好的dom，初始化echarts实例
   const myChart = echarts.init(document.getElementById('main'))
-  const { data } = await proxy.$http.get('reports/type/1')
-  if (data.meta.status !== 200) return proxy.$message.error('获取图表数据失败')
+  // const { data } = await proxy.$http.get('reports/type/1')
+  // if (data.meta.status !== 200) return proxy.$message.error('获取图表数据失败')
   // 准备数据和配置项
-  const newObj = merge(data.data, options)
+  // const newObj = merge(data.data, options)
   // 展示数据
-  myChart.setOption(newObj)
+  // myChart.setOption(newObj)
+  myChart.setOption(sNewObj)
 })
 
 </script>
