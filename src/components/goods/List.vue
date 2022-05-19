6a14ce3c11a9774058fff78f79aad20e7891d034
hongzida <1286629587@qq.com>
2022-05-19 16:03:14 +0800

    添加静态商品列表数据

--- src/components/goods/List.vue
+++ src/components/goods/List.vue
@@ -27,7 +27,7 @@
       </el-col>
     </el-row>
     <el-table
-      :data="list.goodsList"
+      :data="sList"
       :border="true"
       :stripe="true"
     >
@@ -86,6 +86,169 @@ const list = reactive({
   // 保存获取到的商品数据列表
   goodsList: []
 })
+// 静态商品数据列表
+const sList = reactive([
+  {
+    goods_id: 928,
+    cat_id: null,
+    goods_name: '是',
+    goods_price: 1,
+    goods_number: 1,
+    goods_weight: 1,
+    goods_state: null,
+    add_time: 1651686957,
+    upd_time: 1651686957,
+    hot_mumber: 0,
+    is_promote: false,
+    cat_one_id: null,
+    cat_two_id: null,
+    cat_three_id: null
+  },
+  {
+    goods_id: 926,
+    cat_id: null,
+    goods_name: '【海外购自营】黎珐(ReFa) MTG日本 CARAT铂金微电流瘦脸瘦身提拉紧致V脸美容仪 【保税仓发货】',
+    goods_price: 1399,
+    goods_number: 100,
+    goods_weight: 100,
+    goods_state: 0,
+    add_time: 1514345477,
+    upd_time: 1514345477,
+    hot_mumber: 0,
+    is_promote: false,
+    cat_one_id: null,
+    cat_two_id: null,
+    cat_three_id: null
+  },
+  {
+    goods_id: 925,
+    cat_id: null,
+    goods_name: '卡奇莱德汽车车载空气净化器负离子除甲醛PM2.5除烟异味车用氧吧双涡轮出风（红色）',
+    goods_price: 168,
+    goods_number: 100,
+    goods_weight: 100,
+    goods_state: 0,
+    add_time: 1514259452,
+    upd_time: 1514259452,
+    hot_mumber: 0,
+    is_promote: false,
+    cat_one_id: null,
+    cat_two_id: null,
+    cat_three_id: null
+  },
+  {
+    goods_id: 924,
+    cat_id: null,
+    goods_name: '沿途（yantu）车载充电器车充一拖二usb转接口手机智能头多功能汽车点烟器',
+    goods_price: 0,
+    goods_number: 100,
+    goods_weight: 100,
+    goods_state: 0,
+    add_time: 1514259448,
+    upd_time: 1514259448,
+    hot_mumber: 0,
+    is_promote: false,
+    cat_one_id: null,
+    cat_two_id: null,
+    cat_three_id: null
+  },
+  {
+    goods_id: 923,
+    cat_id: null,
+    goods_name: '车载冰箱7.5L 冷暖两用汽车冰箱半导体12V迷你电冰箱升级款',
+    goods_price: 149,
+    goods_number: 100,
+    goods_weight: 100,
+    goods_state: 0,
+    add_time: 1514259445,
+    upd_time: 1514259445,
+    hot_mumber: 0,
+    is_promote: false,
+    cat_one_id: null,
+    cat_two_id: null,
+    cat_three_id: null
+  },
+  {
+    goods_id: 922,
+    cat_id: null,
+    goods_name: '神行者电子狗 神行者L70电子狗测速 测速雷达 流动测速 多种警示路段提醒',
+    goods_price: 98,
+    goods_number: 100,
+    goods_weight: 100,
+    goods_state: 0,
+    add_time: 1514259442,
+    upd_time: 1514259442,
+    hot_mumber: 0,
+    is_promote: false,
+    cat_one_id: null,
+    cat_two_id: null,
+    cat_three_id: null
+  },
+  {
+    goods_id: 921,
+    cat_id: null,
+    goods_name: '360行车记录仪尊享升级版 J501C 安霸A12 高清夜视 WIFI连接 智能管理 机卡套装 黑色(存储卡品牌随机)',
+    goods_price: 359,
+    goods_number: 100,
+    goods_weight: 100,
+    goods_state: 0,
+    add_time: 1514259438,
+    upd_time: 1514259438,
+    hot_mumber: 0,
+    is_promote: false,
+    cat_one_id: null,
+    cat_two_id: null,
+    cat_three_id: null
+  },
+  {
+    goods_id: 920,
+    cat_id: null,
+    goods_name: '淘尔杰汽车专用伸缩蜡拖 除尘车掸子擦车拖把 洗车蜡刷清洁工具用品',
+    goods_price: 39,
+    goods_number: 100,
+    goods_weight: 100,
+    goods_state: 0,
+    add_time: 1514259435,
+    upd_time: 1514259435,
+    hot_mumber: 0,
+    is_promote: false,
+    cat_one_id: null,
+    cat_two_id: null,
+    cat_three_id: null
+  },
+  {
+    goods_id: 919,
+    cat_id: null,
+    goods_name: '保赐利(botny) 防雾剂汽车用长效雾清车窗挡风玻璃浴室去雾剂除雾剂玻璃清洁剂 B-2113',
+    goods_price: 0,
+    goods_number: 100,
+    goods_weight: 100,
+    goods_state: 0,
+    add_time: 1514259432,
+    upd_time: 1514259432,
+    hot_mumber: 0,
+    is_promote: false,
+    cat_one_id: null,
+    cat_two_id: null,
+    cat_three_id: null
+  },
+  {
+    goods_id: 918,
+    cat_id: null,
+    goods_name: '龟牌硬壳蜡新汽车防护划痕去污上光抛光养护漆固体洗车打蜡腊正品',
+    goods_price: 49,
+    goods_number: 100,
+    goods_weight: 100,
+    goods_state: 0,
+    add_time: 1514259429,
+    upd_time: 1514259429,
+    hot_mumber: 0,
+    is_promote: false,
+    cat_one_id: null,
+    cat_two_id: null,
+    cat_three_id: null
+  }
+])
 const total = ref(0)
 // 发起请求，获取商品列表数据
 const getGoodsList = async () => {
@@ -94,7 +257,7 @@ const getGoodsList = async () => {
   proxy.$message.success('获取商品列表信息成功')
   list.goodsList = data.data.goods
   total.value = data.data.total
-  // console.log(data)
+  // console.log(list.goodsList)
 }
 getGoodsList()
 // 监听size变化触发的事件
